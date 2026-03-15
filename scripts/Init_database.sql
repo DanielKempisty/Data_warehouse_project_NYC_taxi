38fe590f3b0720c331b2b19c9da389f9b10e6108
Daniel Kempisty <125575545+DanielKempisty@users.noreply.github.com>
2026-03-15 13:57:51 +0100

    Update Init_database.sql

--- scripts/Init_database.sql
+++ scripts/Init_database.sql
@@ -4,8 +4,8 @@ Create Database and Schemas
 =============================================================
 Script Purpose:
     This script creates a new database named 'NYC_taxi' after checking if it already exists. 
-    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
-    within the database: 'bronze', 'silver', and 'gold'.
+    If the database exists, it is dropped and recreated. Additionally, the script sets up four schemas 
+    within the database: 'bronze', 'silver', 'gold' nad 'etl'.
 	
 WARNING:
     Running this script will drop the entire 'NYC_taxi' database if it exists. 
@@ -40,3 +40,5 @@ GO
 
 CREATE SCHEMA gold;
 GO
+	
+CREATE SCHEMA etl;
